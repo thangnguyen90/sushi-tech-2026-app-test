f6aff8fb88d5707a8a9e45dbcfd64a29631ea671
nhatha <nhatha@bravesoft.com.vn>
2025-12-29 09:28:51 +0700

    feat: implement user authentication with Sanctum and create personal access tokens table

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,60 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Laravel\Sanctum\Http\Middleware\CheckAbilities;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
+use Symfony\Component\HttpFoundation\Request as RequestAlias;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: '/api/v1',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(
+            headers: RequestAlias::HEADER_X_FORWARDED_FOR |
+            RequestAlias::HEADER_X_FORWARDED_HOST |
+            RequestAlias::HEADER_X_FORWARDED_PORT |
+            RequestAlias::HEADER_X_FORWARDED_PROTO |
+            RequestAlias::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'abilities' => CheckAbilities::class,
+            'ability' => CheckForAnyAbility::class,
+        ]);
+        $middleware->group('api', [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (Throwable $e, Request $request) {
+            Log::error($e);
+            if ($e instanceof NotFoundHttpException) {
+                return response()->json([
+                    'result' => [
+                        'code' => 'CMSE404',
+                        'message' => 'エラーが発生しました。もう一度お試しください。',
+                    ],
+                ], 404);
+            }
+            return response()->json([
+                'result' => [
+                    'code' => 'CMSE500',
+                    'message' => 'エラーが発生しました。もう一度お試しください。',
+                ],
+            ], 500);
+        });
+    })
+    ->create();
