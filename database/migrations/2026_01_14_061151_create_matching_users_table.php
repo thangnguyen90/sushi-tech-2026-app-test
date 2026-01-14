de887ca1bc7892d35e8ea8eb29fb1dd957ef85b5
thangnguyen <thangnguyen@bravesoft.vn>
2026-01-14 13:35:51 +0700

    Fix migrate

--- database/migrations/2026_01_14_061151_create_matching_users_table.php
+++ database/migrations/2026_01_14_061151_create_matching_users_table.php
@@ -12,21 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('matching_users', function (Blueprint $table) {
-            Schema::create('matching_users', function (Blueprint $table) {
-                $table->bigIncrements('id');
+            $table->bigIncrements('id');
 
-                $table->unsignedBigInteger('event_id');
+            $table->unsignedBigInteger('event_id');
 
-                $table->unsignedBigInteger('owner_user_id');
-                $table->unsignedBigInteger('peer_user_id');
+            $table->unsignedBigInteger('owner_user_id');
+            $table->unsignedBigInteger('peer_user_id');
 
-                $table->tinyInteger('status')->comment('1.申請中, 2.マッチングリクエスト, 3.マッチング済, 4.商談済');
+            $table->tinyInteger('status')->comment('1.申請中, 2.マッチングリクエスト, 3.マッチング済, 4.商談済');
 
-                $table->timestamps();
-                $table->softDeletes();
+            $table->timestamps();
+            $table->softDeletes();
 
-                $table->index(['owner_user_id', 'status', 'peer_user_id'], 'idx_event_owner_status_peer');
-            });
+            $table->index(['owner_user_id', 'status', 'peer_user_id'], 'idx_event_owner_status_peer');
         });
     }
 
