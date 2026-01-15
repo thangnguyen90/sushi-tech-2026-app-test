3e4c7a1941aeabba9aeb39091bd739686e27c173
nhatha <nhatha@bravesoft.com.vn>
2026-01-15 14:24:41 +0700

    refactor: update namespace for MatchingPartnerController and its usage in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\MatchingPartnerController;
+use App\Http\Controllers\MatchingPartnerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LiveChatProfilesController;
