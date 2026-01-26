87a9e79b3b085765a777dbacff3d1fb9e5c657f0
nhatha <nhatha@bravesoft.com.vn>
2026-01-26 16:14:38 +0700

    update batch

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Services\ResponseService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Process;
+use Illuminate\Http\JsonResponse;
+use App\Services\BusinessApproveWebhookService;
 
 class WebhookController extends Controller
 {
@@ -14,7 +16,7 @@ class WebhookController extends Controller
 
     }
 
-    public function handleCsvListTriggerWebhook(Request $request): \Illuminate\Http\JsonResponse
+    public function handleCsvListTriggerWebhook(Request $request): JsonResponse
     {
         // Handle the webhook logic here
         // For example, process the CSV list trigger
@@ -25,8 +27,16 @@ class WebhookController extends Controller
 //        }
         $data = $request->all();
         foreach ($data as $item) {
-            Process::path(base_path())->start("php artisan $item->name $item->id");
+            Process::path(base_path())->start("php artisan ".$item['name']. ' /'. $item['fileurl']);
         }
         return $this->responseService->success('Webhook processed successfully', 200, status: 201);
     }
+
+//    public function businessApprovement(Request $request): JsonResponse
+//    {
+//        $data = $request->all();
+//        $businessApproveWebhookService = app(BusinessApproveWebhookService::class);
+//        $businessApproveWebhookService->process($data);
+//        return $this->responseService->success('Webhook processed successfully', 200, status: 201);
+//    }
 }
