1cfac3d86a1a084e50cbab50973c9ca8d9b96536
bsv_PhuLy <>
2026-01-27 16:08:27 +0700

    update chat event id

--- resources/js/utils/constantUrl.ts
+++ resources/js/utils/constantUrl.ts
@@ -1,6 +1,6 @@
-import { EVENTOS_CLIENT_URL, EVENTOS_EVENT_ID, EVENTOS_LIVE_CHAT_CONTENT_ID, EVENTOS_PORTAL_ID } from "@/shared/constants/env"
+import { EVENTOS_CLIENT_URL, EVENT_CHAT_EVENT_ID, EVENTOS_LIVE_CHAT_CONTENT_ID, EVENTOS_PORTAL_ID } from "@/shared/constants/env"
 
 export const LiveChatRedirect = (moduleId: string, webLinkId: string) => {
-    const url = `${EVENTOS_CLIENT_URL}/uri/web_link/${EVENTOS_PORTAL_ID}/${EVENTOS_EVENT_ID}/${EVENTOS_LIVE_CHAT_CONTENT_ID}/${moduleId}/${webLinkId}`;
+    const url = `${EVENTOS_CLIENT_URL}/uri/web_link/${EVENTOS_PORTAL_ID}/${EVENT_CHAT_EVENT_ID}/${EVENTOS_LIVE_CHAT_CONTENT_ID}/${moduleId}/${webLinkId}`;
     return url
 }
