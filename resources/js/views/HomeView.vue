68e9f8e948aaefb46577c494bece5305f7875e5d
bsv_PhuLy <>
2026-01-28 14:59:36 +0700

    fix check agreement redirect page

--- resources/js/views/HomeView.vue
+++ resources/js/views/HomeView.vue
@@ -161,6 +161,9 @@ const confirmPolicy = () => {
     mutate(undefined, {
         onSuccess: () => {
             contractModal.value = false;
+            if (isCheckAgreement.value) {
+                window.location.href = LiveChatRedirect(moduleMatchingId, matchingWebLinkId);
+            }
         },
         onError: (error) => {
             console.error(error);
@@ -187,11 +190,14 @@ const notices = ref([
     },
 ])
 
+const isCheckAgreement = ref<boolean>(false);
+
 const toMatchingList = () => {
     if (storeAuth.user?.policy_agreed) {
         window.location.href = LiveChatRedirect(moduleMatchingId, matchingWebLinkId);
         return
     }
+    isCheckAgreement.value = true;
     contractModal.value = true;
 }
 
