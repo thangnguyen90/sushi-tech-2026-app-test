c479ec3cd0c1810691ea841d5c4ad5649d064652
thangnguyen <thangnguyen@bravesoft.vn>
2026-01-30 17:16:20 +0700

    update language in header

--- app/Http/Controllers/MatchingPartnerController.php
+++ app/Http/Controllers/MatchingPartnerController.php
@@ -63,12 +63,10 @@ class MatchingPartnerController extends Controller
 
     public function show(Request $request, int $profile_id): JsonResponse
     {
-        $lang = (string) $request->query('lang', 'jpn');
-        $languageId = (int) $request->query('language_id', 1);
+        $lang = (string) $request->header('lang', 'jpn');
 
         $ctx = [
             'profile_id' => $profile_id,
-            'language_id' => $languageId,
             'lang' => $lang,
         ];
 
