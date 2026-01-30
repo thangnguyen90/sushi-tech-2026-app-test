89b020753fd39e75d741f4be51df81f7fd166ebf
nhatha <nhatha@bravesoft.com.vn>
2026-01-30 11:50:44 +0700

    remove fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    protected $fillable = [
-        'uuid',
-        'policy_agreed',
-    ];
+    protected $guarded = [];
 }
