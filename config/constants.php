bbbed4615702b66ad8f0a94aaf31913a7b8a4731
thangnguyen <thangnguyen@bravesoft.vn>
2026-02-02 16:45:52 +0700

    add config number of items return

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,8 @@
 <?php
 return [
     'NET_WORKING_LIMIT' => 5,
+    'LIMIT_NETWORKING_PER_NAME' => 6,
+    'LIMIT_EXHIBITORS' => 6,
+    'LIMIT_VISITORS' => 6,
     'MATCHING_EXCLUDE_STATUSES' => []
 ];
