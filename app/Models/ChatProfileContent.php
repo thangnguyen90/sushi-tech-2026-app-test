dc7916fb35295356e0c1e1c9087f0e010b534492
thangnguyen <thangnguyen@bravesoft.vn>
2026-02-03 19:20:06 +0700

    delete data unused

--- app/Models/ChatProfileContent.php
+++ app/Models/ChatProfileContent.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ChatProfileContent extends Model
 {
+    use SoftDeletes;
     protected $guarded = [];
     protected $table = 'chat_profile_contents';
 
