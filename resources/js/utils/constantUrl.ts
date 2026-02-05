5d6ee270218108cc1ca6f13b3dcd1ec5b82fd840
bsv_PhuLy <>
2026-02-05 09:28:32 +0700

    update weblink 出展者を探す

--- resources/js/utils/constantUrl.ts
+++ resources/js/utils/constantUrl.ts
@@ -1,4 +1,4 @@
-import { EVENTOS_CLIENT_URL, CHAT_EVENT_ID, EVENTOS_PORTAL_ID, BUSINESS_LINK_ID, BUSINESS_MODULE_ID } from "@/shared/constants/env"
+import { EVENTOS_CLIENT_URL, CHAT_EVENT_ID, EVENTOS_PORTAL_ID, BUSINESS_MODULE_ID, BUSINESS_LINK_ID, EXHIBITOR_MODULE_ID, EXHIBITOR_LINK_ID } from "@/shared/constants/env"
 
 export const LiveChatRedirect = (moduleId: string, webLinkId: string) => {
     const url = `${EVENTOS_CLIENT_URL}/uri/web_link/${EVENTOS_PORTAL_ID}/${CHAT_EVENT_ID}/${moduleId}/${webLinkId}`;
@@ -7,5 +7,10 @@ export const LiveChatRedirect = (moduleId: string, webLinkId: string) => {
 
 export const BusinessWebLink = () => {
     const url = `${EVENTOS_CLIENT_URL}/uri/united_web_link/${EVENTOS_PORTAL_ID}/${CHAT_EVENT_ID}/${BUSINESS_MODULE_ID}/${BUSINESS_LINK_ID}`;
+    return url
+}
+
+export const ExhibitorWebLink = () => {
+    const url = `${EVENTOS_CLIENT_URL}/uri/web_link/${EVENTOS_PORTAL_ID}/${CHAT_EVENT_ID}/${EXHIBITOR_MODULE_ID}/${EXHIBITOR_LINK_ID}`;
     return url
 }
