70dea422578fff118580e89486d5ad42092fe7f7
thangnguyen <thangnguyen@bravesoft.vn>
2026-03-24 14:12:19 +0700

    remove mock setup

--- app/Http/Controllers/MatchingPartnerController.php
+++ app/Http/Controllers/MatchingPartnerController.php
@@ -90,8 +90,7 @@ class MatchingPartnerController extends Controller
 
         $result = $service->buildRecommendResult(
             userUuid: $currentUserUuid,
-            content: $validated['content'],
-            overrideUserUuidList: $validated['user_uuid_list'] ?? []
+            content: $validated['content']
         );
 
         return $this->responseService->success(
