49cf90f399fb754c1c1f770b5f296e414d9ccf35
thangnguyen <thangnguyen@bravesoft.vn>
2026-04-21 18:35:53 +0700

    Fix work break

--- resources/js/views/ReceptionView.vue
+++ resources/js/views/ReceptionView.vue
@@ -1559,6 +1559,7 @@ const completeCheckin = async (appointment: ReceptionAppointment): Promise<void>
 
     &__complete-visitor-line {
         margin-bottom: 0;
+        word-break: break-word;
         //overflow: hidden;
         //text-overflow: ellipsis;
         //white-space: nowrap;
