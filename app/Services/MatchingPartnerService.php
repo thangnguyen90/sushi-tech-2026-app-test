9bb88f2d73129aad33807baa35b8006f5f5c979b
thangnguyen <thangnguyen@bravesoft.vn>
2026-04-29 21:34:21 +0700

    remove user_uuid

--- app/Services/MatchingPartnerService.php
+++ app/Services/MatchingPartnerService.php
@@ -905,7 +905,6 @@ class MatchingPartnerService
             'id',
             'profile_id',
             'uuid',
-            'user_uuid',
             'nickname',
             'company',
             'mail_address',
